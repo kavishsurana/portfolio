1b374fe285839a92cf0a6ba44e682d8142c498ef
Kavish <suranakavish@gmail.com>
2024-04-18 18:24:56 +0530

    Page animation

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,17 @@
+"use client"
+
+import { motion } from "framer-motion";
+
 const AboutPage = () => {
     return (
         <div>
-        <h1>About Page</h1>
+            <motion.div
+                    className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
+                    animate={{ height: "0vh" }}
+                    exit={{ height: "140vh" }}
+                    transition={{ duration: 0.5, ease: "easeOut" }}
+                />
+            <h1>About Page</h1>
         </div>
     );
 }
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,16 @@
+"use client"
+
+import { motion } from "framer-motion";
+
 const ContactPage = () => {
     return (
         <div>
+            <motion.div
+                    className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
+                    animate={{ height: "0vh" }}
+                    exit={{ height: "140vh" }}
+                    transition={{ duration: 0.5, ease: "easeOut" }}
+                />
         <h1>Contact Page</h1>
         </div>
     );
