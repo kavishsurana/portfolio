168a6c24d9980991171c95eac6e1ec009aeb4dfd
Kavish <suranakavish@gmail.com>
2024-04-23 01:30:52 +0530

    navLink error on deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import NavLink from "./navLink";
+import NavLink from "./NavLink";
 import { motion } from "framer-motion";
 
 const links = [
